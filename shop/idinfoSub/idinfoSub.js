134b640eec39e7a89c1b6388d8f0744a0e19c188
DESKTOP-M99U4B0\img4029 <img4029@naver.com>
2023-11-21 00:26:40 +0900

    집에서 js작업

--- shop/idinfoSub/idinfoSub.js
+++ shop/idinfoSub/idinfoSub.js
@@ -25,7 +25,16 @@ for (let i = 1; i < 32; i++) {
     birthDateD.innerHTML += `<option value="${i}">${i}</option>`
 }
 // ==========================================
-
+//우편번호랑 주소 불러오는 함수
+function execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function (data) {
+            document.getElementById('har').value = data.address;
+            document.getElementById('zip').value = data.zonecode;
+        }
+    }).open();
+    document.getElementById('dar').focus();
+}
 
 //메인 CheckBox가 체크되면 서브가 모두 체크됨
 function mainCheckBox(main) {
@@ -66,46 +75,73 @@ function subExecution(main, subCheckBoxAll) {
         });
     }
 };
-mainExecution(Agreement, mainCheckBox);
-mainExecution(Marketing, mainCheckBox);
-subExecution(Agreement, subCheckBoxAll);
-subExecution(Marketing, subCheckBoxAll);
-
-
-
-function informationCheck() {
-    for (let i = 0, j; i < sxBoxInput.length; i++) {
-        if (sxBoxInput[i].checked) j = sxBoxInput[i].value;
+//생년월일 체크하기위한 함수
+function birthDateCheck(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].selected == true) if (array[i].value == "선택") return 1;
     }
+    return 0;
+}
+//생년월일 체크하기위한 함수를 불러와 확인하는 함수
+function birthDate() {
+    let count = 0;
+        birthDateY = document.querySelector(".birthDateY"),
+        birthDateM = document.querySelector(".birthDateM"),
+        birthDateD = document.querySelector(".birthDateD");
+    count += birthDateCheck(birthDateY.children);
+    count += birthDateCheck(birthDateM.children);
+    count += birthDateCheck(birthDateD.children);
+    if (count != 0) {
+        alert('생년월일을 다시 입력해주세요.')
+    }
+}
+//성별을 체크하기 위한 함수
+function sxCheck() {
+    let sx;
+    for (let i = 0; i < sxBoxInput.length; i++) {
+        if (sxBoxInput[i].checked) sx = sxBoxInput[i].value;
+    }
+    console.log(sx);
+    if (sx == "sxnone") alert('성별을 선택해주세요.');
+}
+//가입버튼 누르면 반응 하는 함수
+function informationCheck() {
+    outer :
     for (let i = 0, j = 0; i < idinfoGridValue.length; i++) {
         switch (i) {
             case 2:
-                console.log(idinfoGridValue[2].value);
+                // console.log(idinfoGridValue[2].value);
                 let count = idinfoGridValue[2].value.split('');
                 for (let n = 0; n < count.length; n++) {
                     if (count.length < 8) {
                         alert(`비밀번호는 영문 대소문자/숫자/특수문자를 혼용하여 8~16자를 입력해주십시오.`);
-                        break;
+                        break outer;
                     } else if ((count[n] == " ") || (count[n] == `"`) || (count[n] == `'`)) {
                         alert(`비밀번호에 '(작은따옴표), "(큰따옴표), 공백은 입력 하실 수 없습니다.`);
-                        break;
+                        break outer;
                     }
                 }
                 break;
             case 3:
                 if (idinfoGridValue[3].value != idinfoGridValue[2].value) {
                     alert("비밀번호가 서로 일치하지 않습니다");
+                    break outer;
                 }
+                break;
+            case 4:
+                birthDate(); //생년월일 체크하기위한 함수를 불러와 확인하는 함수 호출
+                sxCheck(); //성별을 체크하기 위한 함수호출
                 break;
             default:
                 break;
         }
     }
 }
-function printName(id) {
-    // const name = document.getElementById(`${id}`).value;
-    // console.log(name);
-}
+mainExecution(Agreement, mainCheckBox);
+mainExecution(Marketing, mainCheckBox);
+subExecution(Agreement, subCheckBoxAll);
+subExecution(Marketing, subCheckBoxAll);
+
 registrationComplete.childNodes[1].addEventListener('click', (e) => {
     e.preventDefault();
 });
@@ -117,7 +153,10 @@ doubleCheck.addEventListener('click', () => {
             alert("『&』,『?』,『%』『#』 또는 공백 문자를 사용하실 수 없습니다.");
             break;
         }
-        if (n == count.length - 1) alert(`${idinfoGridValue[1].value} 사용할수 있는 아이디입니다.`);
+        if (n == count.length - 1) { 
+            alert(`${idinfoGridValue[1].value} 사용할수 있는 아이디입니다.`);
+            idinfoGridValue[1].readOnly = true;
+        } 
     }
 });
 
