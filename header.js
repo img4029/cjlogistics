0c1c1d18a0e5f18e026e48f98e73c5203c7aa8a1
herzuna <img4029@naver.com>
2023-11-23 14:38:31 +0900

    전체 헤더 푸터 수정

--- header.js
+++ header.js
@@ -27,7 +27,40 @@ let testing = document.querySelectorAll('.testing'),
         cdar: "",
         hsp: "",
     };
+function change() {
+    let menuList = document.querySelector('.menu_list'),
+        menuIcon1 = document.querySelector('.menu_icon1'),
+        menuIcon2 = document.querySelector('.menu_icon2'),
+        menuIcon3 = document.querySelector('.menu_icon3'),
+        menuBtn = document.querySelector('#menuBtn');
 
+    if (menuBtn.value == "A") {
+
+        menuList.style.animation = "slidein 0.7s";
+        menuList.style.animationFillMode = "forwards";
+
+        menuIcon1.style.animation = "animate-line-1 1s";
+        menuIcon1.style.animationFillMode = "forwards";
+
+        menuIcon2.style.animation = "animate-line-2 1s";
+        menuIcon2.style.animationFillMode = "forwards";
+
+        menuIcon3.style.animation = "animate-line-3 1s";
+        menuIcon3.style.animationFillMode = "forwards";
+
+        menuBtn.value = "B";
+    } else {
+        menuList.style.animation = "slideout 0.7s";
+
+        menuIcon1.style.animation = "animate-line-4 1s";
+
+        menuIcon2.style.animation = "animate-line-5 1s";
+
+        menuIcon3.style.animation = "animate-line-6 1s";
+
+        menuBtn.value = "A";
+    }
+}
 async function getClientData() {
     try {
         const response = await axios.get('http://localhost:3000/loginComplete/1');
