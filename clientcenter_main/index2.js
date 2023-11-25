d82a45c7068441bf9c08b283970067ab591229cb
agr4005 <144239265+agr4005@users.noreply.github.com>
2023-11-25 22:10:24 +0900

    11/25 고객센터 테이블 클릭 이벤트 완료, 페이징 초기 작업

--- clientcenter_main/index2.js
+++ clientcenter_main/index2.js
@@ -1,47 +1,48 @@
 
-// 검색창 기능
+/*==================== 검색창 구현 ====================*/
 
 function search() {
-    let category = document.querySelector('#category').value.toLowerCase();
-    let keyword = document.querySelector('#search_box').value.toLowerCase();
+    let keyword = document.querySelector('#search_box').value;
 
     let search_row = document.querySelectorAll('#faq_table tbody tr');
+    let check_tr = 0;
 
-
-    for (let row of search_row) {
+    for (let i = search_row.length - 1; i >= 0; i--) {
+        let row = search_row[i];
 
         if (row.classList.contains('faqcell') && row.style.display === 'none') {
             continue;
         }
 
-        let categoryCell = row.children[1].innerHTML.toLowerCase().trim(); 
-        let titleCell = row.children[2].innerHTML.toLowerCase().trim(); 
+        let categoryCell =row.children[1].innerHTML
+        let titleCell = row.children[2].innerHTML;
 
-        if ((category === '' || categoryCell.includes(category)) &&
-            (titleCell.includes(keyword))) {
+        if (((categoryCell.includes(keyword)|| titleCell.includes(keyword)))) {
+            check_tr++;
             row.style.display = 'table-row';
+            row.children[0].textContent = check_tr;
         } else {
             row.style.display = 'none';
         }
-        if(keyword.length < 2) {
-            alert('검색어는 2자이상 입력해주세요.');
-            break;  //검색어가 2자 미만일시 alert, 검색 취소
-    }
+        
+        if (keyword.length < 2) {
+            alert('검색어는 2자 이상 입력해주세요.');
+            break;  // 검색어가 2자 미만일시 alert, 검색 취소
+        }
     }
 }
 
-
-//검색할때 enter 키 활성화 (굳이 클릭할 필요x)
-
+// 검색할 때 enter 키 활성화 (굳이 클릭할 필요x)
 let enterpress = document.getElementById("search_box");
 
-    enterpress.addEventListener("keyup", function (event) {
-      if (event.keyCode === 13) {
+enterpress.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
         document.getElementById("search_button").click();
-      }
-    });
-
-    function changehtml() {
-        let hugefactor = document.getElementsByClassName("client_container");
-        hugefactor.remove();
     }
+});
+
+
+
+
+
+/*==================== 페이지 이동 구현 ====================*/
