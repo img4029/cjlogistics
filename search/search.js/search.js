81d6341182c4bc9cab6dfbdefebf5c2d961213ad
DESKTOP-49QFQN7\KSO <kso77649115@gmail.com>
2023-11-26 23:04:51 +0900

    검색 페이지 11.26

--- search/search.js/search.js
+++ search/search.js/search.js
@@ -85,7 +85,7 @@ let products = [
 
 // html 구현
 // -1 html 클래스 당겨오기
-const productItem = document.querySelector('.item_container');
+let productItem = document.querySelector('.item_container');
 
 // -2 js 활용 브라우저 내 레이아웃 구성 / 이미지 순서 변경을 위한 변수 추가 선언(let)
 for (let i = 0; i < products.length; i++) {
@@ -101,7 +101,105 @@ for (let i = 0; i < products.length; i++) {
     productItem.insertAdjacentHTML("beforeend", product_list);
 }
 
-// 2. 키워드 검색 후 제품 색인
+// 2. 페이지 네이션
+// const contents = document.querySelector(".item_container");
+// const buttons = document.querySelector(".pageButtons");
+
+// const numOfContent = 36;
+// const showButton = 4;
+// const maxContent = 9;
+// const maxPage = Math.ceil(numOfContent / maxContent);
+// let page = 1;
+
+// const makeContent = (id) => {
+//     const NewProducts = [...products];
+//     // i 가 정의되어 있지 않아서 id로 수정
+//     let product_list = `<div class="products_list">
+//         <img src="${NewProducts[id - 1].img}" class="itemImg"> <!-- id로 수정 -->
+//         <a href="#" class="itemName">${NewProducts[id - 1].product}</a> <!-- id로 수정 -->
+//         <a class="itemChar">${NewProducts[id - 1].character}</a> <!-- id로 수정 -->
+//         <a class="itemPrice">${NewProducts[id - 1].price}</a> <!-- id로 수정 -->
+//     </div>`;
+//     productItem.insertAdjacentHTML("beforeend", product_list);
+// };
+
+// const makeButton = (id) => {
+//     const button = document.createElement("button");
+//     button.classList.add("button");
+//     button.dataset.num = id;
+//     button.innerText = id;
+//     button.addEventListener("click", (e) => {
+//         Array.prototype.forEach.call(buttons.children, (button) => {
+//             if (button.dataset.num) button.classList.remove("active");
+//         });
+//         e.target.classList.add("active");
+//         renderContent(parseInt(e.target.dataset.num));
+//     });
+//     return button;
+// };
+
+// const renderContent = (page) => {
+//     // productItem을 정의해야 합니다.
+//     const productItem = document.querySelector(".productItem"); // Assuming ".productItem" is the container for your content
+
+//     // 목록 리스트 초기화
+//     while (productItem.hasChildNodes()) {
+//         productItem.removeChild(productItem.lastChild);
+//     }
+//     // 글의 최대 개수를 넘지 않는 선에서, 화면에 최대 10개의 글 생성
+//     for (let id = (page - 1) * maxContent + 1; id <= page * maxContent && id <= numOfContent; id++) {
+//         makeContent(id); // 수정된 부분: renderContent 내에서 makeContent 직접 호출
+//     }
+// };
+
+// const renderButton = (page) => {
+//     // 버튼 리스트 초기화
+//     while (buttons.hasChildNodes()) {
+//         buttons.removeChild(buttons.lastChild);
+//     }
+//     // 화면에 최대 5개의 페이지 버튼 생성
+//     for (let id = page; id < page + showButton && id <= maxPage; id++) {
+//         buttons.appendChild(makeButton(id));
+//     }
+//     // 첫 버튼 활성화(class="active")
+//     buttons.children[0].classList.add("active");
+
+//     // productItem이 정의되어 있지 않아서 수정
+//     const productItem = document.querySelector(".productItem");
+//     productItem.prepend(prev);
+//     productItem.append(next);
+
+//     // 이전, 다음 페이지 버튼이 필요한지 체크
+//     if (page - showButton < 1) productItem.removeChild(prev); // 수정된 부분: buttons가 아니라 productItem
+//     if (page + showButton > maxPage) productItem.removeChild(next); // 수정된 부분: buttons가 아니라 productItem
+// };
+
+// const render = (page) => {
+//     renderContent(page);
+//     renderButton(page);
+// };
+// render(page);
+
+// const goPrevPage = () => {
+//     page -= showButton;
+//     render(page);
+// };
+
+// const goNextPage = () => {
+//     page += showButton;
+//     render(page);
+// };
+
+// const prev = document.createElement("button");
+// prev.classList.add("button", "prev");
+// prev.innerHTML = '<ion-icon name="chevron-back-outline"></ion-icon>';
+// prev.addEventListener("click", goPrevPage);
+
+// const next = document.createElement("button");
+// next.classList.add("button", "next");
+// next.innerHTML = '<ion-icon name="chevron-forward-outline"></ion-icon>';
+// next.addEventListener("click", goNextPage);
+
 
 // 3. 제품 가격별 정렬(오름차순, 내림차순) : 제품 모두 삭제 후 정렬 순서대로 신규 삽입
 // -1 오름차순(낮은)
@@ -154,4 +252,32 @@ lowPriceBtn.addEventListener('click', itemHighSort);
 
 
 
-// 4. 제품 가격대 검색받아 제품 색인
+//  4. 검색 필터
+
+// const searchInput = document.querySelector('.searchBox1');
+// const searchButton = document.querySelector('.searchButton');
+
+// searchButton.addEventListener('click', () => {
+//     const keyword = searchInput.value.toLowerCase();
+
+//     // 목록 리스트 초기화
+//     while (productItem.hasChildNodes()) {
+//         productItem.removeChild(productItem.firstChild);
+//     }
+
+//     // 검색어에 맞는 제품만 표시
+//     const filteredProducts = products.filter(product => {
+//         return product.product.toLowerCase().includes(keyword);
+//     });
+
+//     // 검색 결과 표시
+//     filteredProducts.forEach(product => {
+//         let product_list = `<div class="products_list">
+//             <img src="${product.img}" class="itemImg">
+//             <a href="#" class="itemName">${product.product}</a>
+//             <a class="itemChar">${product.character}</a>
+//             <a class="itemPrice">${product.price}</a>
+//         </div>`;
+//         productItem.insertAdjacentHTML("beforeend", product_list);
+//     });
+// });
