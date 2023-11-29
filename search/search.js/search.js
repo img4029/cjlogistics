3edf82e4c40d47ccaf9c7823cb52c8a6ab9ee7b0
DESKTOP-49QFQN7\KSO <kso77649115@gmail.com>
2023-11-29 22:14:08 +0900

    검색페이지 주석 수정 완료

--- search/search.js/search.js
+++ search/search.js/search.js
@@ -14,26 +14,17 @@ async function getClientData1() {
         // 제품 수량 안내 문구
         searchInfo = document.querySelector(".searchInfo");
 
-        // 제품 카테고리별 배열로 담아주는 변수
+        // 카테고리별 제품을 배열로 담아주는 변수
         allData =
             [...productData.perfume,
             ...productData.diffuser,
             ...productData.candle,
             ...productData.homeCare];
 
-        // 함수 참조를 통해 전체 데이터 브라우저에 출력
-        // getItem(allData);
-
-
         // N개의 제품 안내 문구의 길이 및 문자열 리터럴 활용
         searchInfo.innerText = `${allData.length} 개의 상품이 있습니다`
 
-        // 콘솔로 배열 확인
-        console.log(allData);
-
-        // 검색..ㅠ.ㅠ
-        // ======================================================
-        // 내림차순 오름차순 함수 호출 및 perfumeData 매개변수 참조
+        // 내림차순 오름차순 함수 호출
         highPriceBtn.addEventListener('click', () => {
             itemLowSort(allData);
         });
@@ -41,8 +32,9 @@ async function getClientData1() {
             itemHighSort(allData);
         });
 
+        // 페이지 네이션 함수 호출
         pageNation(allData, currentPage);
-
+        // 검색 함수 호출
         searchKey(allData);
 
 
@@ -50,15 +42,14 @@ async function getClientData1() {
         console.log('데이터를 가져오는 중 오류 발생');
         console.log(err.message);
     }
-
 };
 getClientData1();
 
-// 선택한 제품페이지 정보를 서버에 저장하는 함수
+// 선택한 제품 상세페이지 정보를 서버에 저장
 async function station(product) {
     try {
         console.log(product);
-        let response1 = await axios.post('http://localhost:3000/station',product);
+        let response1 = await axios.post('http://localhost:3000/station', product);
 
     } catch (err) {
         console.log('데이터를 가져오는 중 오류 발생');
@@ -66,109 +57,51 @@ async function station(product) {
     }
 }
 
-// html 구현
-
-// 1. html 클래스 당겨오기
+// 1. 정렬 : HTML에 지정한 Class 요소에 대한 참조
 let productItem = document.querySelector('.item_container');
-// 객체의 참조를 받아서 배열로 된 데이터를 html 형태로 구현
-function getItem(products) {
-    for (let i = 0; i < products.length; i++) {
-        let changeImg = products[i].img;
-        let product_list = `<div class="products_list">
-            <img src="${changeImg}" class="itemImg">
-            <a href="#" class="itemName">${products[i].productName}</a>
-            <a class="itemChar">${products[i].character}</a>
-            <a class="itemPrice">${products[i].price.toLocaleString() + '원'
-            }</a>
-        </div>`;
-
-        // for문, 배열 활용 products 객체 참조
-        productItem.insertAdjacentHTML("beforeend", product_list);
-    }
-};
-
-// -2 js 활용 브라우저 내 레이아웃 구성 / 이미지 순서 변경을 위한 변수 추가 선언(let)
-
-// 2. 제품 가격별 정렬(오름차순, 내림차순) : 제품 모두 삭제 후 정렬 순서대로 신규 삽입
-// -1 오름차순(낮은)
 const highPriceBtn = document.querySelector('.highPrice');
 const lowPriceBtn = document.querySelector('.lowPrice');
 
+// 1) 오름차순 : sort 메서드 활용
 function itemHighSort(productData) {
-    console.log(productData);
 
     productItem.innerHTML = '';
-    // while (productItem.hasChildNodes()) {
-    //     productItem.removeChild(productItem.firstChild);
-    // }
     const NewProducts = [...productData];
-    console.log(NewProducts);
+
     NewProducts.sort(function (a, b) {
         return parseFloat(a.price) - parseFloat(b.price);
     });
+
     pageNation(NewProducts, currentPage);
-    // 별도로 products_list를 생성하여 반복문을 사용하였으나
-    // for (let i = 0; i < NewProducts.length; i++) {
-    //     let product_list = `<div class="products_list">
-    //         <img src="${NewProducts[i].img}" class="itemImg">
-    //         <a href="#" class="itemName">${NewProducts[i].productName}</a>
-    //         <a class="itemChar">${NewProducts[i].character}</a>
-    //         <a class="itemPrice">${NewProducts[i].price.toLocaleString() + '원'
-    //         }</a>
-    //     </div>`;
-    //     productItem.insertAdjacentHTML("beforeend", product_list);
-    // }
-    // return NewProducts;
 }
 
-// -2 내림차순(높은가격부터~)
+// 2) 내림차순 : sort 메서드 활용
 function itemLowSort(productData) {
-    console.log(productData);
 
     productItem.innerHTML = '';
-    // while (productItem.hasChildNodes()) {
-    //     productItem.removeChild(productItem.firstChild);
-    // }
+
     const NewProducts = [...productData];
+
     NewProducts.sort(function (a, b) {
         return parseFloat(b.price) - parseFloat(a.price);
     });
+
     pageNation(NewProducts, currentPage);
-    // for (let i = 0; i < NewProducts.length; i++) {
-    //     let product_list = `<div class="products_list">
-    //         <img src="${NewProducts[i].img}" class="itemImg">
-    //         <a href="#" class="itemName">${NewProducts[i].productName}</a>
-    //         <a class="itemChar">${NewProducts[i].character}</a>
-    //         <a class="itemPrice">${NewProducts[i].price.toLocaleString() + '원'
-    //         }</a>
-    //     </div>`;
-    //     productItem.insertAdjacentHTML("beforeend", product_list);
-    // }
-    // return NewProducts;
 }
 
 
 
-//  3. 검색 필터
-// 검색하는 키워드의 값을 담아줄 변수
-
-// 위에서 전체 배열에 대한 데이터를 매개변수로 받음
-
-// html 인풋박스 값, 서치 버튼 요소 소환
-// 키워드를 찾아서 데이터를 배열 형태로 모음
+//  2. 검색 필터 : HTML에 지정한 Class 요소에 대한 참조
 let searchKeyword = document.querySelector('#searchBox1');
 let searchButton = document.querySelector('.searchButton');
 let searchText = document.querySelector('.searchText1');
 
+// 1) 키워드 색인
 function searchKey(allData) {
-    console.log(allData);
-    // 새로받을 데이터
 
-    // 클릭 시 입력받은 키워드 배열 선언
     searchButton.addEventListener('click', () => {
         let selectData = [];
-        const keyword = searchKeyword.value  // 입력받을 키워드 변수 선언
-
+        const keyword = searchKeyword.value
 
         for (let i = 0; i < allData.length; i++) {
             if (allData[i].productName.includes(keyword)) {
@@ -184,15 +117,13 @@ function searchKey(allData) {
     });
 }
 
+// 2) 색인된 키워드 정렬
 function selectDatafunc(selectData) {
-    console.log(selectData);
 
     productItem.innerHTML = '';
-    // while (productItem.hasChildNodes()) {
-    //     productItem.removeChild(productItem.firstChild);
-    // }
 
     for (let i = 0; i < selectData.length; i++) {
+
         let changeImg = selectData[i].img
         let product_list = `<div class="products_list">
         <img src="${changeImg}" class="itemImg">
@@ -202,39 +133,30 @@ function selectDatafunc(selectData) {
             }</a>
     </div>`;
 
-
         productItem.insertAdjacentHTML("beforeend", product_list);
     }
 };
 
+// 3. 페이지 네이션
 
-
-// console.log(selectDatafunc());
-//  이거므ㅓ이;ㅁ 불러오질 못하고계속ㄱ오류나
-
-// 2. 페이지 네이션
-// 컨텐츠는 기존 컨텐츠 끌어다 쓰면되고, 버튼은 동적으로 만들어야됨
+// HTML에 지정한 Class 요소에 대한 참조
 productItem = document.querySelector(".item_container");
 
-// count 변수를 초기에 실행 후 페이지 네이션 함수 실행 후
-// if문에서 ==0으로 동일하지 않아 
+// 최상위에서 count 변수를 초기화 후 페이지 네이션 함수 실행 : 함수 안에서 초기화 시 페이지 버튼 무한 생성 유의할 것
 let count = 0;
 
 function pageNation(allData, currentPage) {
 
-
     const pageContent = 9;
-    const showbutton = 5;
     const totalContent = allData.length;
-    console.log(allData);
     const totalPage = Math.ceil(totalContent / pageContent);
-    // for문 안에서 초기화 시 페이지가 계속 이동하니까 유의할것...
+
     productItem.innerHTML = '';
 
+    // 1) 페이지 내 9개의 컨텐츠 노출
     for (let i = pageContent * (currentPage - 1); i < (pageContent * (currentPage - 1)) + pageContent; i++) {
 
         let changeImg = allData[i].img;
-
         let product_list = `<div class="products_list">
         <img src="${changeImg}" class="itemImg">
         <a href="#" class="itemName">${allData[i].productName}</a>
@@ -244,25 +166,32 @@ function pageNation(allData, currentPage) {
 
         productItem.insertAdjacentHTML("beforeend", product_list);
     };
+
+    // 2) 컨텐츠 클릭 후 상세페이지로 이동 구현
     let products_list = document.querySelectorAll(".products_list");
     for (let i = 0; i < products_list.length; i++) {
-        products_list[i].addEventListener('click',(e)=>{
+
+        products_list[i].addEventListener('click', (e) => {
             station(allData[i])
             location.href = '../LHN/DetailedPage.html';
         })
     }
-    let buttons = document.querySelector(".pageButtons");
 
+    // 3) 페이지 네이션 버튼 요소 생성
+    let buttons = document.querySelector(".pageButtons");
     if (count == 0) {
+
         for (let j = 1; j <= totalPage; j++) {
             let button_list = `<div class="button_list">${j}</div>`
             buttons.insertAdjacentHTML("beforeend", button_list);
             count++;
         }
     }
-    let button_list = document.querySelectorAll(".button_list");
 
+    // 4) 버튼 클릭 후 페이지 이동 구현
+    let button_list = document.querySelectorAll(".button_list");
     for (let k = 0; k < button_list.length; k++) {
+
         button_list[k].addEventListener('click', (e) => {
             currentPage = e.target.innerText;
             pageNation(allData, currentPage)
